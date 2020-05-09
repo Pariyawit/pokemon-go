905221e4a19414dd74e33c7d50b80b466a240e92
Pariyawit Jatukannyaprateep <pariyawit.jat@gmail.com>
2020-05-09 17:26:53 +1000

    add server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const path = require('path');
+
+const app = express();
+dotenv.config();
+
+//Middleware
+app.use(express.json());
+
+// Serve statics assets if in production
+if (process.env.NODE_ENV === 'production') {
+  //Set statci folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
